935c3146955cd70d5e325056866e8a98ab4ace91
a232319779 <529531385@qq.com>
2017-01-11 17:57:27 +0800

    test other RSL_*

--- src/host/gprsdecode/main.c
+++ src/host/gprsdecode/main.c
@@ -119,16 +119,19 @@ void process_handle_burst(struct l1ctl_burst_ind *bi)
 
 	switch (type) {
 	case RSL_CHAN_Bm_ACCHs:
-	    if ((ts > 0) && ((fn % 13) != 12))
-		process_pdch(bi, DEBUG_PRINT);
+	    if ((ts > 0) && ((fn % 13) != 12))	// 12 and 25 is the last one. should be discard. 
+			process_pdch(bi, DEBUG_PRINT);
 		break;
 	case RSL_CHAN_Lm_ACCHs:
+		printf("It RSL_CHAN_Lm_ACCHs!\n");
+		break;
 	case RSL_CHAN_BCCH:
 	case RSL_CHAN_SDCCH4_ACCH:
 	case RSL_CHAN_SDCCH8_ACCH:
 	case RSL_CHAN_RACH:
 	case RSL_CHAN_PCH_AGCH:
 	default:
+		printf("Type not handle!\n");
 		break;
 	}
 }
